003853fd69d0114aa5a2b32913b6f4b053c457b7
NEBULA1978 <rpascualey@gmail.com>
2023-05-09 12:40:09 +0200

    Endpoints probados con Thunder client

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import express from 'express'
 
 const app = express()
 
+// Enpoins o Middelwers
+// Para ver en web:
+// http://localhost:3000/employees
+app.get('/employees', (req,res) => res.end('obteniendo empleados'))
+
+app.post('/employees', (req,res) => res.end('creando empleados'))
+
+app.put('/employees', (req,res) => res.end('actualizando empleados'))
+
+app.delete('/employees', (req,res) => res.end('eliminando empleados'))
+
+
 app.listen(3000)
 console.log('Server runnig on port 3000')
 
